0fa4b6091de8bfb534d8ab9dc4705c4cbd448a9b
Jeanne <Jeanne.Choo@bankofsingapore.com>
2020-12-09 12:07:17 +0800

    day 2 update

--- 2/passwords.py
+++ 2/passwords.py
@@ -1,6 +1,7 @@
 import pandas as pd 
 from collections import Counter
 
+# Part 1
 with open('2-input.txt', 'r') as f:
     input = f.readlines()
     input = [i.replace('\n', '') for i in input]
@@ -19,3 +20,12 @@ for index, row in input_df.iterrows():
     if string_hash[row['letter']] in range(row['start'], row['end']+1):
         valid.append(index)
 print(len(valid))
+
+# Part 2 
+valid = 0
+for index, row in input_df.iterrows():
+    char_a = row['string'][row['start']-1]
+    char_b = row['string'][row['end']-1]
+    char_hash = Counter([char_a, char_b])
+    if char_hash.get(row['letter'], -1) == 1:
+        valid += 1
