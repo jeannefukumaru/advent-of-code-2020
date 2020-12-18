43b02b1fb6acc2db0c6e9202061ca23c1621b9da
Jeanne <jiawenchoo@gmail.com>
2020-12-18 15:50:27 +0800

    tweak to game.py

--- game.py
+++ game.py
@@ -1,24 +1,21 @@
 with open('input.txt', 'r') as file:
-    input = file.readlines()
-    input = [i.replace('\n', '') for i in input]
-    input = [{i.split()[0]:i.split()[1]} for i in input]
+    inp = file.readlines()
+    inp = [i.replace('\n', '') for i in inp]
+    inp = [{i.split()[0]:i.split()[1]} for i in inp]
 
-inp = [{'nop':'+0'}, {'acc':'+1'}, {'jmp':'+4'},{'acc':'+3'},{'jmp': '-3'},{'acc':'-99'}, {'acc':'+1'},{'jmp':'-4'},{'acc': '+6'}]
 seen = []
 idx = 0
 acc = 0
 while idx not in seen:
-    for k, v in input[idx].items():
+    for k, v in inp[idx].items():
         if k == 'acc':
+            seen.append(idx)
             acc += int(v)
-            print(acc)
             idx += 1
-            print(idx)
-            seen.append(idx)
         elif k == 'jmp':
+            seen.append(idx)
             idx += int(v)
-            seen.append(idx)
         elif k == 'nop':
-            idx += 1
             seen.append(idx)
+            idx += 1
 print(acc)
